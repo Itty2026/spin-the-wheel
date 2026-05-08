199c3f3c51263ef056fc4f82f7964413fd31a377
Itty2026 <ielewitz@specialadvantage.org>
2026-05-08 14:16:36 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCtrMEOBqhiYZuufgqy3zN1-t3RkwjTijU",
   authDomain: "spin-the-wheel-app-75887.firebaseapp.com",
@@ -6,5 +5,4 @@ const firebaseConfig = {
   storageBucket: "spin-the-wheel-app-75887.firebasestorage.app",
   messagingSenderId: "348559150557",
   appId: "1:348559150557:web:a852e91a1551554d337a34",
-  measurementId: "G-GLJ6XP774P"
 };
