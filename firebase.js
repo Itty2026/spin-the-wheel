f2af83c707866d1103def56e7581c605a57ad277
Itty2026 <ielewitz@specialadvantage.org>
2026-05-08 14:19:39 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCtrMEOBqhiYZuufgqy3zN1-t3RkwjTijU",
+  apiKey: "AIzaSyCtrMEOBqhiYZuufgqy3zN1-t3RkwjTiJU",
   authDomain: "spin-the-wheel-app-75887.firebaseapp.com",
   projectId: "spin-the-wheel-app-75887",
   storageBucket: "spin-the-wheel-app-75887.firebasestorage.app",
   messagingSenderId: "348559150557",
-  appId: "1:348559150557:web:a852e91a1551554d337a34",
+  appId: "1:348559150557:web:a852e91a1551554d337a34"
 };
